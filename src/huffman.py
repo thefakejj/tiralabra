0a62c3a1b86b998f43933f7a37ddcfbd2238694c
thefakejj <jasper.laurila@gmail.com>
2026-01-19 23:26:32 +0200

    nyt huffman-koodit on ainakin kohillaan

--- src/huffman.py
+++ src/huffman.py
@@ -1,4 +1,21 @@
+import heapq
+
 #Huffman-koodaus
+
+class Node:
+    def __init__(self, freq: int, char=None):
+        self.freq = freq
+        self.char = char
+        self.code = None
+        self.left = None
+        self.right = None
+
+    def add_huff_code(self, code: str):
+        self.code = code
+
+    # less-than operaaito, jotta heap osaa järjestellä nodet
+    def __lt__(self, other):
+        return self.freq < other.freq
 
 def create_freq_table(text: str):
     freq_table = {}
@@ -8,3 +25,75 @@ def create_freq_table(text: str):
         else:
             freq_table[char] += 1
     return freq_table
+
+def create_tree(freq_table: dict):
+    prio_queue = []
+
+    for char in freq_table:
+        freq = freq_table[char]
+        node = Node(freq, char)
+        heapq.heappush(prio_queue, node)
+
+    while len(prio_queue) >= 2:
+        left = heapq.heappop(prio_queue)
+        right = heapq.heappop(prio_queue)
+
+        new_node = Node(left.freq+right.freq)
+        new_node.left = left
+        new_node.right = right
+        heapq.heappush(prio_queue, new_node)
+
+    root = heapq.heappop(prio_queue)
+    return root
+
+# rekursiivinen funktio jolla muodostetaan puun lehdille huffman-koodit
+def set_huffman_codes(node, codes: list, current: str):
+
+    if node is None:
+        return
+    
+    if node.left is None and node.right is None:
+        codes.append(current)
+        node.add_huff_code(current)
+        return
+
+    set_huffman_codes(node.left, codes, current+"0")
+    set_huffman_codes(node.right, codes, current+"1")
+
+def huffman_codes_to_characters_connection(root, freq_table: dict):
+    codes = []
+    set_huffman_codes(root, codes, "")
+    huff_codes = {}
+    char_i = 0
+    bfs(root)
+    # for char in freq_table.keys():
+    #     huff_codes[char] = codes[char_i]
+    #     char_i += 1
+    # print(huff_codes)
+   # return huff_codes
+
+
+
+
+
+
+
+
+
+
+
+def bfs(root):
+    visited = set()
+    queue = [root]
+    while len(queue)>0:
+        current = queue[0]
+        if current not in visited and not None:
+            for child in [current.left, current.right]:
+                if child != None:
+                    queue.append(child)
+            visited.add(current)
+            queue.remove(current)
+        #print(current.freq)
+        #trying huff code attribute for leaves
+        if current.left is None and current.right is None:
+            print(current.char, current.code, current.freq)
--- src/main.py
+++ src/main.py
@@ -1,4 +1,4 @@
-from huffman import create_freq_table
+from huffman import create_freq_table, create_tree, huffman_codes_to_characters_connection
 
 def get_text_from_file(path):
     with open(path) as f:
@@ -6,4 +6,8 @@ def get_text_from_file(path):
 
 if __name__ in "__main__":
     filetext = get_text_from_file("./src/sampletexts/johndoe.txt")
-    print(create_freq_table(filetext))
+    freq_table = create_freq_table(filetext)
+    #print(freq_table)
+    tree = create_tree(freq_table)
+    huffman_codes = huffman_codes_to_characters_connection(tree, freq_table)
+    print(huffman_codes)

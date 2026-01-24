76a9d4970715a4bdd0692b0d1ad50acd15011f13
thefakejj <jasper.laurila@gmail.com>
2026-01-24 17:05:58 +0200

    what

--- src/main.py
+++ src/main.py
@@ -1,19 +1,31 @@
 from huffman import codelist_to_text, create_huffman_string, create_freq_table, create_tree, huffman_codes_to_characters_connection
 
+from lz import lz
+
 def get_text_from_file(path):
     with open(path) as f:
         return f.read()
 
 if __name__ in "__main__":
-    filetext = get_text_from_file("./src/sampletexts/johndoe.txt")
-    freq_table = create_freq_table(filetext)
-    #print(freq_table)
-    tree = create_tree(freq_table)
-    huffman_codes, chars = huffman_codes_to_characters_connection(tree)
-    #print(huffman_codes["T"])
-    output_string, codelist = create_huffman_string(filetext, huffman_codes)
-    # print(filetext)
-    #print(output_string)
-    #print(codelist)
-    original_text = codelist_to_text(codelist, chars)
-    print(original_text)
+
+    funnytext = "ABRACADABRARABARABARA"
+    lz_dict = lz(funnytext)
+    print(lz_dict)
+
+    funnytext = "ABRACADABRARABAREABARAE"
+    lz_dict = lz(funnytext)
+    print(lz_dict)
+
+    # HUFFMAN
+    #filetext = get_text_from_file("./src/sampletexts/johndoe.txt")
+    # freq_table = create_freq_table(filetext)
+    # #print(freq_table)
+    # tree = create_tree(freq_table)
+    # huffman_codes, chars = huffman_codes_to_characters_connection(tree)
+    # #print(huffman_codes["T"])
+    # output_string, codelist = create_huffman_string(filetext, huffman_codes)
+    # # print(filetext)
+    # #print(output_string)
+    # #print(codelist)
+    # original_text = codelist_to_text(codelist, chars)
+    # print(original_text)
